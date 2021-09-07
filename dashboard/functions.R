71b06dd6bf0ea46b25addfd168434cc043fe5ad4
Joe Brew <joebrew@gmail.com>
2021-09-07 14:15:05 +0200

    minor

--- dashboard/functions.R
+++ dashboard/functions.R
@@ -5,7 +5,8 @@ library(knitr)
 library(flextable)
 library(lubridate)
 
-load_data <- function(){
+#' @param modify_variable_names If TRUE, then variable names will be overwritten / modified to be more meaningful
+load_data <- function(modify_variable_names = FALSE){
   # Read in data
   owd <- getwd()
   setwd('../')
@@ -53,28 +54,60 @@ load_data <- function(){
   # • All arboviruses 2015-2020 (cases and deaths) (q55, b, c): trends if possible
   # • Most recent year data (q56)
   
+  keep_questions <- c('11', '11b',
+                      '5', 
+                      '12',
+                      '28', '41',
+                      '10', '24',
+                      '6',
+                      '7',
+                      '9',
+                      '15', '17',
+                      '19',
+                      '6', '21',
+                      '35',
+                      '37', '37b', '38',
+                      '39',
+                      '49',
+                      '34', '34b', '34c',
+                      '28',
+                      '28e',
+                      '34', '34b', '34c',
+                      '55', '55b', '55c',
+                      '56')
+  df <- data[,dict$`Variable name`[dict$`Question number` %in% keep_questions]]
   
-  df <- data[,dict$`Variable name`[dict$`Question number` %in% c('11', '11b',
-                                                                 '5', 
-                                                                 '12',
-                                                                 '28', '41',
-                                                                 '10', '24',
-                                                                 '6',
-                                                                 '7',
-                                                                 '9',
-                                                                 '15', '17',
-                                                                 '19',
-                                                                 '6', '21',
-                                                                 '35',
-                                                                 '37', '37b', '38',
-                                                                 '39',
-                                                                 '49',
-                                                                 '34', '34b', '34c',
-                                                                 '28',
-                                                                 '28e',
-                                                                 '34', '34b', '34c',
-                                                                 '55', '55b', '55c',
-                                                                 '56')]]
+  # Modify variable names
+  if(modify_variable_names){
+    sub_dict <- dict %>%
+      filter(`Question number` %in% keep_questions)
+    
+    # Go variable by variable and get a new field name
+    show_it <- function(x, condition = NULL){
+      if(is.null(condition)){
+        ifelse(!is.na(x), 
+               x, 
+               '')
+      } else {
+        ifelse(!is.na(condition),
+               x,
+               '')
+      }
+    }
+    sub_dict <- sub_dict %>%
+      mutate(field_name = paste0(show_it(`Question number`),
+                                 show_it('. ', condition = `Question number`),
+                                 show_it(' ', condition = Description),
+                                 show_it(`Description`),
+                                 show_it(' ', condition = `Subquestion`),
+                                 show_it(`Subquestion`)))
+    for(i in 1:nrow(sub_dict)){
+      this_variable_name <- sub_dict$`Variable name`[i]
+      this_column_index <- which(names(df) == this_variable_name)
+      this_new_name <- sub_dict$field_name[i]
+      names(df)[this_column_index] <- this_new_name
+    }
+  }
   return(df)
 }
 
