0ad9d2e367d8c25be072d15b4c50c06ab2937439
Matthew Rudd <mbrudd@sewanee.edu>
2021-11-30 21:14:33 -0600

    recovered report script

--- create_country_reports.R
+++ create_country_reports.R
@@ -7,15 +7,15 @@ if( file.exists("data/data.RData")){
 
 countries <- data$SI01
 
-if(!dir.exists('reports')){
-  dir.create('reports')
+if(!dir.exists('country_reports')){
+  dir.create('country_reports')
 }
 
 for(i in 1:length( countries )){
   nom <- countries[i]
   if( !is.na( nom ) ){
-    report_name <- paste0("reports/",nom,".pdf")
-    rmarkdown::render( "report.Rmd", 
+    report_name <- paste0("country_reports/",nom,".pdf")
+    rmarkdown::render( "country_report.Rmd", 
                        params=list(country=nom), 
                        output_file = report_name )
   }
