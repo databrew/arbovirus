4d2d2ac82c929ef5771be175e251cfeb4f703ced
Matthew Rudd <mbrudd@sewanee.edu>
2022-02-19 21:52:22 -0600

    tabbed regional tables

--- create_region_reports.R
+++ create_region_reports.R
@@ -1,3 +1,4 @@
+library(readr)
 
 if( file.exists("data/data.RData")){
   load("data/data.RData")
@@ -12,8 +13,8 @@ if(!dir.exists('region_reports')){
 }
 
 # create reports
-for(i in 1:length( regions )){
-  nom <- regions[i]
+for(j in 1:length( regions )){
+  nom <- regions[j]
   if( !is.na( nom ) ){
     report_name <- paste0("region_reports/",nom,".html")
     rmarkdown::render( "region_report.Rmd", 
@@ -24,17 +25,44 @@ for(i in 1:length( regions )){
   }
 }
 
+# filename is assumed to have extension .Rmd
+tabs_html <- function( filename ){
+  region_question <- unlist( strsplit( filename , ".", fixed=TRUE))[1]
+  region_question <- unlist( strsplit( region_question, "_", fixed=TRUE))
+  region <- region_question[1]
+  question <- region_question[2]
+  
+  html <- '\n<ul class="custom_tabbar">\n'
+  for(j in 1:length( regions )){
+    if( regions[j] == region ){
+      html <- paste0( html, '<li class="custom_tabbar__list-active"><a href="' )
+    } else{
+      html <- paste0( html,'<li><a href="' )
+    }
+    html <- paste0( html,
+                    regions[j], '_', question,
+                    '.html">',
+                    regions[j],
+                    '</a></li>\n' )
+  }
+  html <- paste0( html, '</ul>\n' )
+  return( html )
+}
+
 # create RMDs for dashboard
-for(i in 1:length( regions )){
-  region <- regions[i]
+for(j in 1:length( regions )){
+  region <- regions[j]
   filenames <- dir("dashboard")[ grep(region, dir("dashboard")) ]
-  for(j in 1:length(filenames)){
-    path <- paste0("dashboard/",filenames[j])
-    frontmatter <- read_file( paste0("dashboard/region_chunk_frontmatter.Rmd"))
-    frontmatter <- paste0(frontmatter,"\n```{r}\ndata <- data %>% filter(Region==", region, ")\n```\n")
-    tabs <- read_file( paste0("dashboard/", region, ".html"))
+  for(k in 1:length(filenames)){
+    path <- paste0("dashboard/",filenames[k])
+    frontmatter <- read_file("chunk_frontmatter.Rmd")
+    frontmatter <- paste0(frontmatter,"\n```{r}\ndata <- data %>% filter(Region=='", region, "') %>% droplevels() %>% mutate(SI01 = as.character(SI01)) %>% arrange( SI01 )\n```\n")
+    tabs <- tabs_html( filenames[k] )
     chunk <- read_file(path)
     contents <- paste0(frontmatter, tabs, "\n```{r}\n", chunk, "\n```\n")
     write_file(contents, path, append=FALSE)
   }
 }
+
+setwd("dashboard")
+rmarkdown::render_site()
