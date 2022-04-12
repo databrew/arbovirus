e7d8dbc49ef80601161697f283480d209bf8b636
Matthew Rudd <mbrudd@sewanee.edu>
2022-04-12 15:57:59 -0500

    dashboard updates

--- new_dashboard/create_national_pages.R
+++ new_dashboard/create_national_pages.R
@@ -15,7 +15,7 @@ menu_start <- function( id ){
 }
 
 submenu_start <- function(){
-  return("<div class='col-sm-3 text-left'>\n<ul class='submenu-text-formatting'>\n")
+  return("<div class='col-sm-6 text-left'>\n<ul class='submenu-text-formatting'>\n")
 }
 
 submenu_href_title <- function( region, suffix, text ){
@@ -28,6 +28,18 @@ submenu_title <- function( text ){
 
 submenu_item <- function( region, suffix, text ){
   return( paste0("<li><a href='", region, "_", suffix, ".html'>", text, "</a></li>\n") )
+}
+
+submenu_dropdown <- function( child, submenu, text ){
+  return( paste0("<li><h4>\n",
+                 "<a role='button' data-toggle='collapse' href='#",
+                 child,
+                 "' aria-expanded='false' aria-controls='",
+                 submenu,
+                 "'>",
+                 text,
+                 "</a>\n",
+                 "</h4></li>\n"))
 }
 
 submenu_end <- function(){
@@ -44,16 +56,18 @@ header_content <- function( region, page ){
   content <- "<div class='flex-container'>\n"
   content <- paste0(content, 
                     "<h1 class='logo'><img id='WHO_logo' src='img/WHO_Logo.png'></h1>\n",
-                    "<ul class='navigation'>\n",
-                    "<li><a>Select</a></li>\n<li><a>the</a></li>\n<li><a>region(s)</a></li>\n<li><a>of</a></li>\n<li><a>interest:</a></li>\n")
+                    "<div class='dropdown navigation'>\n",
+                    "<button onclick='myFunction()' class='dropbtn'>Selected region:\n",
+                    "<span class='selected_region'>", region, "</span>\n",
+                    "<span class='selected_region_arrow_down'>&#8681;</span>\n",
+                    "</button>\n",
+                    "<div id='myDropdown' class='dropdown-content'>\n")
   for(i in 1:length(regions)){
-    if( regions[i]==region){
-      content <- paste0(content, "<li><a class='region_active' href=''>", regions[i], "</a></li>\n")
-    } else{
-      content <- paste0(content, "<li><a href='", paste0(regions[i], "_", page, ".html"), "'>", regions[i], "</a></li>\n")
+    if( regions[i]!=region){
+      content <- paste0(content, "<a href='", regions[i], "_", page, ".html'>", regions[i], "</a>\n")
     }
   }
-  content <- paste0( content, "<li><a>&nbsp;</a></li>\n</ul>\n</div>\n\n" )
+  content <- paste0( content, "</div>\n</div>\n</div>\n\n" )
   
   # navbar 
   content <- paste0(content, 
@@ -63,66 +77,105 @@ header_content <- function( region, page ){
                     "<a role='button' data-toggle='collapse' href='#overview-navigation' aria-expanded='false' aria-controls='overview-navigation'>Arbovirus survey overview</a>\n",
                     "</li>\n<li id='Regional-Li-area'>\n",
                     "<a role='button' data-toggle='collapse' href='#regional-navigation' aria-expanded='false' aria-controls='regional-navigation'>Regional arbovirus survey results</a>\n",
-                    # "</li>\n<li id = 'Country-Li-area'>\n",
-                    # "</li>\n<li>\n<a role='button' data-toggle='collapse' href='", region, "_national.html'>National arbovirus survey results</a>\n",
                     "</li>\n<li>\n<a href='", region, "_national.html'>National arbovirus survey results</a>\n",
-                    # "</li>\n<li>\n<a role='button' data-toggle='collapse' href='", region, "_about.html'>About</a>\n</li>\n</ul>\n\n")
                     "</li>\n<li>\n<a href='", region, "_about.html'>About</a>\n</li>\n</ul>\n\n")
   
   # drop-down menus
   # Overview NAV
   content <- paste0( content,
                      menu_start("overview-navigation"), 
-                     submenu_start(), submenu_href_title(region, "context", "Survey context"), submenu_end(),
-                     submenu_start(), submenu_href_title(region, "participation", "Participating countries"), submenu_end(),
+                     submenu_start(), 
+                     submenu_href_title(region, "context", "Survey context"),
+                     submenu_href_title(region, "participation", "Participating countries"), 
+                     submenu_end(),
                      menu_end() )
-  # Regional NAV
+  
+  # Regional drop-down parent menus
   content <- paste0( content,
                      menu_start("regional-navigation"),
                      submenu_start(), 
-                     submenu_title("Arboviral disease surveillance system"),
+                     submenu_dropdown("regional-navigation-disease-surveillance",
+                                      "regional-navigation",
+                                      "Arboviral disease surveillance system"),
+                     submenu_dropdown("regional-navigation-lab-capacity",
+                                      "regional-navigation",
+                                      "Arbovirus laboratory capacity"),
+                     submenu_dropdown("regional-navigation-management",
+                                      "regional-navigation",
+                                      "Management of arboviral disease cases"),
+                     submenu_dropdown("regional-navigation-vector-surveillance",
+                                      "regional-navigation",
+                                      "Routine vector surveillance and control"),
+                     submenu_href_title(region, "Q41", "Animal surveillance"),
+                     submenu_dropdown("regional-navigation-community",
+                                      "regional-navigation",
+                                      "Community sensitization and participation"),
+                     submenu_dropdown("regional-navigation-preparedness",
+                                      "regional-navigation",
+                                      "Preparedness for arboviral outbreaks/epidemics"),
+                     submenu_dropdown("regional-navigation-surveillance-staffing",
+                                      "regional-navigation",
+                                      "Surveillance staffing"),
+                     submenu_end(),
+                     menu_end() )
+  
+  # Regional drop-down child menus
+  content <- paste0( content,
+                     menu_start("regional-navigation-disease-surveillance"),
+                     submenu_start(),
                      submenu_item(region, "Q5", "Autochthonous (local mosquito-borne) circulation"),
                      submenu_item(region, "Q6", "Arbovirus surveillance administration and planning"), 
                      submenu_item(region, "Q11", "Disease reporting requirements and practices"),
                      submenu_item(region, "Q14", "Successes and barriers/challenges"),
-                     submenu_title("Surveillance staffing"),
-                     submenu_item(region, "Q58", "Surveillance staff capacity"), 
-                     submenu_item(region, "Q61", "Arboviral expertise resources"),
                      submenu_end(),
+                     menu_end(), 
+                     menu_start("regional-navigation-lab-capacity"),
                      submenu_start(),
-                     submenu_title("Arbovirus laboratory capacity"),
                      submenu_item(region, "Q15", "Testing practices (cases tested and viruses targeted)"), 
                      submenu_item(region, "Q17", "Testing capacity"),
                      submenu_item(region, "Q19", "Virological surveillance"), 
                      submenu_item(region, "Q20", "Successes and barriers/challenges"),
-                     submenu_title("Management of arboviral disease cases"),
+                     submenu_end(),
+                     menu_end(), 
+                     menu_start("regional-navigation-management"),
+                     submenu_start(),
                      submenu_item(region, "Q21", "Clinical guidelines and training"), 
                      submenu_item(region, "Q25", "Successes and barriers/challenges"),
                      submenu_end(),
+                     menu_end(), 
+                     menu_start("regional-navigation-vector-surveillance"),
                      submenu_start(),
-                     submenu_title("Routine vector surveillance and control"),
                      submenu_item(region, "Q26", "Entomologic surveillance administration and reporting"), 
                      submenu_item(region, "Q28", "Mosquito surveillance practices"), 
                      submenu_item(region, "Q34", "Vector distribution and transmission risk"), 
                      submenu_item(region, "Q35", "Vector control practices"), 
                      submenu_item(region, "Q37", "Control plans, thresholds, and indicators"), 
                      submenu_item(region, "Q40", "Successes and barriers/challenges"), 
-                     submenu_href_title(region, "Q41", "Animal surveillance"),
                      submenu_end(),
+                     menu_end(),
+                     menu_start("regional-navigation-community"),
                      submenu_start(),
-                     submenu_title("Community sensitization and participation"),
                      submenu_item(region, "Q43", "Community outreach programme capacity"), 
                      submenu_item(region, "Q44", "Public notification of arboviral disease risk and control"), 
                      submenu_item(region, "Q46", "Successes and barriers/challenges"), 
-                     submenu_title("Preparedness for arboviral outbreaks/epidemics"),
+                     submenu_end(),
+                     menu_end(),
+                     menu_start("regional-navigation-preparedness"),
+                     submenu_start(),
                      submenu_item(region, "Q47", "Outbreak response planning and infrastructure"), 
                      submenu_item(region, "Q49", "Outbreak declaration and emergency measures"),
                      submenu_item(region, "Q54", "Successes and barriers/challenges"),
                      submenu_end(),
+                     menu_end(),
+                     menu_start("regional-navigation-surveillance-staffing"),
+                     submenu_start(),
+                     submenu_item(region, "Q58", "Surveillance staff capacity"), 
+                     submenu_item(region, "Q61", "Arboviral expertise resources"),
+                     submenu_end(),
                      menu_end() )
   
   # National NAV
-    
+  
   return( content )
 }
 
